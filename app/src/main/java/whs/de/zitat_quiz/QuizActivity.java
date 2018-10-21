af452746000ed507508ab57a05eb7be2e0f1f3a3
Enthöfer Joshua <j.enthoefer@laudert.de>
2018-10-21 13:06:11 +0200

    - rename "Database" to "DatabaseUtils" - QuizActivity displays quote from chosen category

--- app/src/main/java/whs/de/zitat_quiz/QuizActivity.java
+++ app/src/main/java/whs/de/zitat_quiz/QuizActivity.java
@@ -2,6 +2,7 @@ package whs.de.zitat_quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        TextView txtQuestion = findViewById(R.id.quizText);
+        txtQuestion.setText(DatabaseUtils.pullQuestionText(0));
     }
 }
